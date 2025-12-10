a48948eac7669cbc41a737eeee48fb57bda17e91
Neko Collector <nekocollector@terminal.com>
2025-12-10 10:09:35 +0800

    :3

--- v_dance2.lua
+++ v_dance2.lua
@@ -1,13 +1,15 @@
-local Players = game:GetService("Players")
-local RunService = game:GetService("RunService")
-local StarterGui = game:GetService("StarterGui")
-local TweenService = game:GetService("TweenService")
-local HttpService = game:GetService("HttpService")
-local UserInputService = game:GetService("UserInputService")
-local TextService = game:GetService("TextService")
-local TextChatService = game:GetService("TextChatService")
-local ContextActionService = game:GetService("ContextActionService")
-local Debris = game:GetService("Debris")
+cloneref = cloneref or function(o) return o end
+
+local Debris = cloneref(game:GetService("Debris"))
+local Players = cloneref(game:GetService("Players"))
+local RunService = cloneref(game:GetService("RunService"))
+local StarterGui = cloneref(game:GetService("StarterGui"))
+local HttpService = cloneref(game:GetService("HttpService"))
+local TextService = cloneref(game:GetService("TextService"))
+local TweenService = cloneref(game:GetService("TweenService"))
+local TextChatService = cloneref(game:GetService("TextChatService"))
+local UserInputService = cloneref(game:GetService("UserInputService"))
+local ContextActionService = cloneref(game:GetService("ContextActionService"))
 
 local Player = Players.LocalPlayer
 
