e53c90cba40feb70ee05691d689fd3c968d3eebd
Neko Collector <nekocollector@terminal.com>
2025-12-21 17:22:53 +0800

    :3

--- v_dance2.lua
+++ v_dance2.lua
@@ -18,4 +18,31 @@ local function AddModule(m)
 	table.insert(modules, m)
 end
 
+AddModule(function()
+	local m = {}
+	m.ModuleType = "DANCE"
+	m.Name = "It's Going Down"
+	m.Description = "first dance in v_dance2.lua"
+	m.Assets = {"GoingDown.anim", "GoingDown.mp3"}
+
+	m.Config = function(parent: GuiBase2d)
+	end
+
+	local animator = nil
+	m.Init = function(figure: Model)
+		SetOverrideDanceMusic(AssetGetContentId("GoingDown.mp3"), "IT'S GOING DOWN", 1)
+		animator = AnimLib.Animator.new()
+		animator.rig = figure
+		animator.looped = true
+		animator.track = AnimLib.Track.fromfile(AssetGetPathFromFilename("GoingDown.anim"))
+	end
+	m.Update = function(dt: number, figure: Model)
+		animator:Step(GetOverrideDanceMusicTime())
+	end
+	m.Destroy = function(figure: Model?)
+		animator = nil
+	end
+	return m
+end)
+
 return modules
