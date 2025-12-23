678325aa0ef97f8701e6ed21f0fb779b50237283
Neko Collector <nekocollector@terminal.com>
2025-12-23 20:09:48 +0800

    :3

--- v_moveset1.lua
+++ v_moveset1.lua
@@ -1941,7 +1941,7 @@ AddModule(function()
 				core.Color = curcolor
 				core.CFrame = hole
 				if throt > 0.02 then
-					Effect({Time = math.random(35, 55), EffectType = "Sphere", Size = Vector3.new(0.5, 0.5, 0.5), SizeEnd = Vector3.new(1, 1, 1), Transparency = 0, TransparencyEnd = 1, CFrame = hole, MoveToPos = hole.Position + Vector3.new(math.random(-10, 10), math.random(-10, 10), math.random(-10,1 0)), Material = "Neon", Color = Color3.new(1, 0, 0), Boomerang = 50, BoomerangSize = 50})
+					Effect({Time = math.random(35, 55), EffectType = "Sphere", Size = Vector3.new(0.5, 0.5, 0.5), SizeEnd = Vector3.new(1, 1, 1), Transparency = 0, TransparencyEnd = 1, CFrame = hole, MoveToPos = hole.Position + Vector3.new(math.random(-10, 10), math.random(-10, 10), math.random(-10, 10)), Material = "Neon", Color = Color3.new(1, 0, 0), Boomerang = 50, BoomerangSize = 50})
 				end
 				throt += task.wait()
 			until os.clock() - s > 0.85 or not rootu:IsDescendantOf(workspace)
