c7b9753cd192abbd91cb9c3956bf7f1d1bd2938e
airy-one-1 <146495140+airy-one-1@users.noreply.github.com>
2026-02-01 18:29:46 -0800

    Update v_dance2.lua

--- content/v_dance2.lua
+++ content/v_dance2.lua
@@ -379,12 +379,12 @@ AddModule(function()
 
 	local animator = nil
 	m.Init = function(figure: Model)
-		SetOverrideDanceMusic(AssetGetContentId("Lux.mp3"), "i cant deny it", 1, NumberRange.new(0, 5.88))
+		SetOverrideDanceMusic(AssetGetContentId("imissthequiet.mp3"), "i cant deny it", 1, NumberRange.new(0, 5.88))
 		animator = AnimLib.Animator.new()
 		animator.rig = figure
 		animator.looped = false
 		animator.map = {{0, 5.88}, {0, 5.69999}}
-		animator.track = AnimLib.Track.fromfile(AssetGetPathFromFilename("Lux.anim"))
+		animator.track = AnimLib.Track.fromfile(AssetGetPathFromFilename("imissthequiet.anim"))
 	end
 	m.Update = function(dt: number, figure: Model)
 		animator:Step(GetOverrideDanceMusicTime())
