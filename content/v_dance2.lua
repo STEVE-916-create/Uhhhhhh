26842d56a6112a53157315a19d4174ea2823c706
Alternate Internet <maytheuniversehavemercyinme@alternateinternet.dos.tthp>
2026-02-09 13:31:06 +0800

    :3

--- content/v_dance2.lua
+++ content/v_dance2.lua
@@ -79,7 +79,7 @@ AddModule(function()
 	m.ModuleType = "DANCE"
 	m.Name = "Birdbrain"
 	m.Description = "hey teto you should wish that you could\nc- c- c- c- c- CUHHHH\nFAHHHHHH\n\nSo... ####!!"
-	m.Assets = {"Birdbrain.anim", "Birdbrain.mp3", "BirdbrainAlt.mp3"}
+	m.Assets = {"Birdbrain.anim", "BirdbrainLaggy.anim", "Birdbrain.mp3", "BirdbrainAlt.mp3"}
 
 	m.Lag = false
 	m.Config = function(parent: GuiBase2d)
@@ -106,15 +106,14 @@ AddModule(function()
 		animator = AnimLib.Animator.new()
 		animator.rig = figure
 		animator.looped = false
-		animator.track = AnimLib.Track.fromfile(AssetGetPathFromFilename("Birdbrain.anim"))
+		if m.Lag then
+			animator.track = AnimLib.Track.fromfile(AssetGetPathFromFilename("BirdbrainLaggy.anim"))
+		else
+			animator.track = AnimLib.Track.fromfile(AssetGetPathFromFilename("Birdbrain.anim"))
+		end
 	end
 	m.Update = function(dt: number, figure: Model)
-		if m.Lag then
-			local fps = 1 / 12
-			animator:Step((GetOverrideDanceMusicTime() // fps) * fps)
-		else
-			animator:Step(GetOverrideDanceMusicTime())
-		end
+		animator:Step(GetOverrideDanceMusicTime())
 	end
 	m.Destroy = function(figure: Model?)
 		animator = nil
@@ -411,7 +410,7 @@ AddModule(function()
 	local m = {}
 	m.ModuleType = "DANCE"
 	m.Name = "Egypt"
-	m.Description = "imagine getting banned here"
+	m.Description = "imagine getting banned here\nimproved now"
 	m.Assets = {"Egypt.anim", "Egypt.mp3"}
 
 	m.Config = function(parent: GuiBase2d)
@@ -420,17 +419,18 @@ AddModule(function()
 	local animator = nil
 	local start = 0
 	m.Init = function(figure: Model)
-		SetOverrideDanceMusic(AssetGetContentId("Egypt.mp3"), "Mofe. - Prince of Egypt", 1)
+		SetOverrideDanceMusic(AssetGetContentId("Egypt.mp3"), "Mofe. - Prince of Egypt", 1, NumberRange.new(0, 24.481))
 		start = os.clock()
 		animator = AnimLib.Animator.new()
 		animator.rig = figure
 		animator.looped = true
 		animator.speed = 1
+		animator.map = {{0, 24.481}, {0, 24}}
 		animator.track = AnimLib.Track.fromfile(AssetGetPathFromFilename("Egypt.anim"))
 	end
 	m.Update = function(dt: number, figure: Model)
 		local t = os.clock()
-		animator:Step(t - start)
+		animator:Step(GetOverrideDanceMusicTime())
 	end
 	m.Destroy = function(figure: Model?)
 		animator = nil
@@ -442,7 +442,7 @@ AddModule(function()
 	local m = {}
 	m.ModuleType = "DANCE"
 	m.Name = "Rambunctious"
-	m.Description = "idk"
+	m.Description = "rare forty nighty emote\ngot me feelin rambunctious!!!"
 	m.Assets = {"Rambunctious.anim", "Rambunctious.mp3"}
 
 	m.Config = function(parent: GuiBase2d)
@@ -451,20 +451,229 @@ AddModule(function()
 	local animator = nil
 	local start = 0
 	m.Init = function(figure: Model)
-		SetOverrideDanceMusic(AssetGetContentId("Rambunctious.mp3"), "PatQuinnMusic - Rambunctious", 1)
+		SetOverrideDanceMusic(AssetGetContentId("Rambunctious.mp3"), "PatQuinnMusic - Rambunctious", 1, NumberRange.new(0, 7.341))
 		start = os.clock()
 		animator = AnimLib.Animator.new()
 		animator.rig = figure
 		animator.looped = true
 		animator.speed = 1
+		animator.map = {{0, 7.341}, {0, 3.67 * 2}}
 		animator.track = AnimLib.Track.fromfile(AssetGetPathFromFilename("Rambunctious.anim"))
 	end
 	m.Update = function(dt: number, figure: Model)
 		local t = os.clock()
-		animator:Step(t - start)
+		animator:Step(GetOverrideDanceMusicTime())
 	end
 	m.Destroy = function(figure: Model?)
 		animator = nil
+	end
+	return m
+end)
+
+if game.Players.LocalPlayer.Name == "STEVETheReal916" then return modules end
+
+AddModule(function()
+	local m = {}
+	m.ModuleType = "DANCE"
+	m.Name = "FNAF Remix"
+	m.Description = "most violent jumpscare ive ever seen btw :sob:"
+	m.Assets = {"FNAFFreddy.anim", "FNAFFreddyIdle.anim", "FNAFFreddy.mp3"}
+
+	m.FullVersion = false
+	m.Effects = true
+	m.Config = function(parent: GuiBase2d)
+		Util_CreateSwitch(parent, "Effects", m.Effects).Changed:Connect(function(val)
+			m.Effects = val
+		end)
+		Util_CreateSwitch(parent, "Idled", m.FullVersion).Changed:Connect(function(val)
+			m.FullVersion = val
+		end)
+	end
+	m.LoadConfig = function(save: any)
+		m.FullVersion = not not save.FullVersion
+		m.Effects = not save.NoEffects
+	end
+	m.SaveConfig = function()
+		return {
+			FullVersion = m.FullVersion,
+			NoEffects = not m.Effects
+		}
+	end
+
+	local animator1 = nil
+	local animator2 = nil
+	m.Init = function(figure: Model)
+		if m.FullVersion then
+			SetOverrideDanceMusic(AssetGetContentId("FNAFFreddy.mp3"), "i found this on instagram", 1)
+		else
+			SetOverrideDanceMusic(AssetGetContentId("FNAFFreddy.mp3"), "i found this on instagram", 1, NumberRange.new(87.258, 110.875))
+			SetOverrideDanceMusicTime(87.258)
+		end
+		animator1 = AnimLib.Animator.new()
+		animator1.rig = figure
+		animator1.looped = true
+		animator1.track = AnimLib.Track.fromfile(AssetGetPathFromFilename("FNAFFreddy.anim"))
+		animator1.map = {{87.258, 110.875}, {0, 5.87 * 2}}
+		animator2 = AnimLib.Animator.new()
+		animator2.rig = figure
+		animator2.looped = false
+		animator2.track = AnimLib.Track.fromfile(AssetGetPathFromFilename("FNAFFreddyIdle.anim"))
+		if textsandstuff then textsandstuff:Destroy() end
+		if m.Effects then
+			textsandstuff = Instance.new("Part")
+			textsandstuff.Transparency = 1
+			textsandstuff.Anchored = true
+			textsandstuff.CanCollide = false
+			textsandstuff.CanTouch = false
+			textsandstuff.CanQuery = false
+			textsandstuff.Name = "INTERNETANGEL"
+			textsandstuff.Size = Vector3.new(10, 6, 0) * figure:GetScale()
+			local sgui = Instance.new("SurfaceGui")
+			sgui.LightInfluence = 0
+			sgui.Brightness = 1
+			sgui.AlwaysOnTop = false
+			sgui.MaxDistance = 1000
+			sgui.SizingMode = Enum.SurfaceGuiSizingMode.FixedSize
+			sgui.CanvasSize = Vector2.new(300, 120)
+			sgui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+			sgui.Name = "UI"
+			sgui.Parent = textsandstuff
+			local text = Instance.new("TextLabel")
+			text.Position = UDim2.new(0, 0, 0, 0)
+			text.Size = UDim2.new(0.5, 0, 0.5, 0)
+			text.BackgroundTransparency = 1
+			text.ClipsDescendants = true
+			text.Font = Enum.Font.Code
+			text.TextColor3 = Color3.new(1, 1, 1)
+			text.TextXAlignment = Enum.TextXAlignment.Left
+			text.TextYAlignment = Enum.TextYAlignment.Center
+			text.TextScaled = true
+			text.Text = "Five\nNights\nat\nFreddy's"
+			text.Name = tostring(id)
+			text.Parent = sgui
+		end
+	end
+	m.Update = function(dt: number, figure: Model)
+		local t = GetOverrideDanceMusicTime()
+		if t >= 87.258 and t <= 110.875 then
+			animator1:Step(t)
+		else
+			animator2:Step(t)
+		end
+		local root = figure:FindFirstChild("HumanoidRootPart")
+		if not root then return end
+		local scale = figure:GetScale()
+		if textsandstuff then
+			textsandstuff.CFrame = root.CFrame * CFrame.new(0, 0, -1 * scale)
+		end
+	end
+	m.Destroy = function(figure: Model?)
+		animator1 = nil
+		animator2 = nil
+		if textsandstuff then textsandstuff:Destroy() textsandstuff = nil end
+	end
+	return m
+end)
+
+AddModule(function()
+	local m = {}
+	m.ModuleType = "DANCE"
+	m.Name = "Looping The Rooms"
+	m.Description = "is there really an escape in this world\nim trapped help\nfah"
+	m.Assets = {"LoopingTheRooms.anim", "LoopingTheRooms.mp3"}
+
+	m.Config = function(parent: GuiBase2d)
+	end
+
+	local animator = nil
+	m.Init = function(figure: Model)
+		SetOverrideDanceMusic(AssetGetContentId("LoopingTheRooms.mp3"), "rusino - Looping the Rooms ft. Blue Teto", 1)
+		animator = AnimLib.Animator.new()
+		animator.rig = figure
+		animator.looped = false
+		animator.speed = 1
+		animator.track = AnimLib.Track.fromfile(AssetGetPathFromFilename("LoopingTheRooms.anim"))
+	end
+	m.Update = function(dt: number, figure: Model)
+		local t = GetOverrideDanceMusicTime()
+		animator:Step(t)
+		local rj = figure:FindFirstChild("HumanoidRootPart") and figure.HumanoidRootPart:FindFirstChild("RootJoint")
+		if rj then
+			if t >= 0.617 and t < 6.55 then
+				rj.Transform -= Vector3.new(18.265 / 2, 0, 0) * figure:GetScale()
+			end
+			if t >= 6.55 and t < 12.517 then
+				rj.Transform -= Vector3.new(12.631 / 2, 0, 0) * figure:GetScale()
+			end
+			if t >= 18.45 and t < 24.43 then
+				rj.Transform -= Vector3.new(12.631 / 2, 0, 0) * figure:GetScale()
+			end
+		end
+	end
+	m.Destroy = function(figure: Model?)
+		animator = nil
+	end
+	return m
+end)
+
+AddModule(function()
+	local m = {}
+	m.ModuleType = "DANCE"
+	m.Name = "Popipo"
+	m.Description = "i know you want my vegetable juice"
+	m.Assets = {"PopipoDance.anim", "PopipoShake.anim", "Popipo.rbxm", "Popipo.mp3"}
+
+	m.Effects = true
+	m.Config = function(parent: GuiBase2d)
+		Util_CreateSwitch(parent, "Effects", m.Effects).Changed:Connect(function(val)
+			m.Effects = val
+		end)
+	end
+	m.LoadConfig = function(save: any)
+		m.Effects = not save.NoEffects
+	end
+	m.SaveConfig = function()
+		return {
+			NoEffects = not m.Effects
+		}
+	end
+
+	local animator1 = nil
+	local animator2 = nil
+	local instances = {}
+	m.Init = function(figure: Model)
+		SetOverrideDanceMusic(AssetGetContentId("Popipo.mp3"), "po pi po pi po po pi po", 1)
+		animator1 = AnimLib.Animator.new()
+		animator1.rig = figure
+		animator1.looped = true
+		animator1.track = AnimLib.Track.fromfile(AssetGetPathFromFilename("PopipoDance.anim"))
+		animator1.map = {{87.258, 110.875}, {0, 5.87 * 2}}
+		animator2 = AnimLib.Animator.new()
+		animator2.rig = figure
+		animator2.looped = false
+		animator2.track = AnimLib.Track.fromfile(AssetGetPathFromFilename("PopipoShake.anim"))
+		for _,v in instances do v:Destroy() end
+		if m.Effects then
+		end
+	end
+	m.Update = function(dt: number, figure: Model)
+		local t = GetOverrideDanceMusicTime()
+		if t >= 87.258 and t <= 110.875 then
+			animator1:Step(t)
+		else
+			animator2:Step(t)
+		end
+		local root = figure:FindFirstChild("HumanoidRootPart")
+		if not root then return end
+		local scale = figure:GetScale()
+		if textsandstuff then
+			textsandstuff.CFrame = root.CFrame * CFrame.new(0, 0, -1 * scale)
+		end
+	end
+	m.Destroy = function(figure: Model?)
+		animator1 = nil
+		animator2 = nil
+		for _,v in instances do v:Destroy() end
 	end
 	return m
 end)
